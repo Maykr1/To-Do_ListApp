a0cb00c0a5c12098282996ddccb335924b5fe5c6
Maykr1 <eclark72713@gmail.com>
2025-05-09 18:19:52 -0400

    Added Spring Files

---
+++ src/test/java/com/project/To_Do_ListApp/ToDoListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.To_Do_ListApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
